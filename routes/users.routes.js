52c02997c50ee97db960854970bce0a82a0c0d78
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-09-11 10:27:39 -0500

    user crud

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -4,6 +4,18 @@ const bcrypt = require("bcrypt");
 // models
 const User = require("../models/User");
 
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.json(other);
+  } catch (err) {
+    next(err);
+  }
+});
+
 router.put("/:id", async (req, res) => {
   if (req.body.userId === req.params.id || req.body.isAdmin) {
     if (req.body.password) {
@@ -24,10 +36,29 @@ router.put("/:id", async (req, res) => {
       );
       res.status(200).json(user);
     } catch (err) {
-      return res.status(500).json(err);
+      next(err);
     }
   } else {
-    return res.status(403).json("You can update only your account!");
+    return res
+      .status(403)
+      .json({ message: "You can update only your account!" });
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+
+  if (req.body.userId === id || req.body.isAdmin) {
+    try {
+      await User.findByIdAndDelete(id);
+      res.status(204).end();
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return res
+      .status(403)
+      .json({ message: "You can delete only your account!" });
   }
 });
 
